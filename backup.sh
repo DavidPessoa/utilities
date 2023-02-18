e0d17c1c56a6a49ecfc1a23966fb9e390fe97d23
David Parreira <davidpessoaparreira@gmail.com>
2023-02-18 18:11:11 +0100

    Updating docker-compose and backup script

--- backup.sh
+++ backup.sh
@@ -4,11 +4,6 @@ FILE_NAME=homeserver-$(date +"%Y-%m-%d").tar.bz2
 
 cd /nas
 
-# #Copy dir to backup and exclude unnecessary folders
-# rsync -av /nas/* /nas/backup/compression/ --exclude /media-center/data --exclude /backup/ --exclude /media-center/docker/jellyfin-config/cache --exclude "media-center/docker/jellyfin-config/transcodes" --exclude "media-center/docker/plex-config/Library/Application Support/Plex Media Server/Cache" --exclude "media-center/docker/sonarr-config/MediaCover" --exclude "media-center/docker/readarr-config/MediaCover" --exclude "media-center/docker/radarr-config/MediaCover" --exclude "media-center/docker/plex-config/Library/Application Support/Plex Media Server/Media" --exclude "media-center/docker/jellyfin-config/data/metadata/People"
-
-cd /nas
-
 tar --exclude='media-center/data' \
     --exclude='media-center/docker/jellyfin-config/cache' \
     --exclude='media-center/docker/jellyfin-config/transcodes' \
@@ -19,13 +14,19 @@ tar --exclude='media-center/data' \
     --exclude='media-center/docker/emby-config/cache' \
     --exclude='media-center/docker/sonarr-config/MediaCover' \
     --exclude='media-center/docker/radarr-config/MediaCover' \
+    --exclude='media-center/docker/emby-config/transcoding-temp' \
+    --exclude='media-center/docker/jellyfin-config/plugins/configurations/intros/cache' \
+    --exclude='smarthome/docker/node-red-config/node_modules' \
+    --exclude='smarthome/docker/node-red-config/.npm/_cacache' \
     --exclude='backup' \
-    -cvpjf $FILE_NAME * 
+    -cpjf $FILE_NAME * 
 
-# cp $FILE_NAME backup/google-drive
+cp $FILE_NAME backup/
 
-# rm $FILE_NAME
+rm $FILE_NAME
 
-# export HOME=/home/homeserver
+cd backup 
 
-# sudo bash /home/homeserver/butdr/cron_backup.bash
+ls -tp | grep -v '/$' | tail -n +8 | xargs -I {} rm -- {}
+
+rclone sync /nas/backup/ homeserver:backup
