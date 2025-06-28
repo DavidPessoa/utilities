9505d518fb0e37e3220ae8ceacf53c3fcc468617
David Parreira <davidpessoaparreira@gmail.com>
2025-06-28 10:54:05 +0200

    Adding immich

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo useradd grafana -u 13018
 sudo useradd portainer -u 13019
 sudo useradd fireflyiii -u 13020
 sudo useradd nginx -u 13040
+sudo useradd immich -u 13043
 
 sudo groupadd utilities -g 130010
 sudo usermod -a -G utilities prometheus
@@ -14,10 +15,11 @@ sudo usermod -a -G utilities portainer
 sudo usermod -a -G utilities homeserver
 sudo usermod -a -G utilities fireflyiii
 sudo usermod -a -G utilities nginx
+sudo usermod -a -G utilities immich
 
 # Make directories
-sudo mkdir -pv docker/{prometheus,grafana,portainer,fireflyiii,nginx}-config
-sudo mkdir -pv data/{prometheus,grafana,portainer,fireflyiii,nginx}
+sudo mkdir -pv docker/{prometheus,grafana,portainer,fireflyiii,nginx,immich}-config
+sudo mkdir -pv data/{prometheus,grafana,portainer,fireflyiii,nginx,immich}
 
 # Set permissions
 sudo chmod -R 775 data/
@@ -29,5 +31,7 @@ sudo chown -R grafana:utilities data/grafana
 sudo chown -R portainer:utilities docker/portainer-config
 sudo chown -R fireflyiii:utilities docker/fireflyiii-config
 sudo chown -R nginx:utilities docker/nginx-config
+sudo chown -R immich:utilities docker/immich-config
+sudo chown -R immich:utilities data/immich
 
 echo "UID=$(id -u)" >> .env
